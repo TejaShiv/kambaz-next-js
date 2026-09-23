273d83c607d44ad64bf169a201a8310676cdba4a
Tejas Shivaprasad <tejas.shivaprasad29@gmail.com>
2026-09-23 06:04:06 -0400

    Add text fields, textarea, and radio buttons form components

--- app/labs/lab1/Images.tsx
+++ app/labs/lab1/Images.tsx
@@ -10,6 +10,13 @@ export default function Images() {
         <br />
         <img id="wd-teslabot" src="/images/2.png" height="200px" alt="Sample image two" />
         <br />
+        An image that matters to me:
+        <br />
+        <img id="wd-your-image" src="/images/1.png" width="300px" alt="A picture I saved" />
+        <br />
+        An extra sample image:
+        <br />
+        <img id="wd-ai-image" src="/images/2.png" width="200px" alt="Another sample image" />
       </div>
     );
   }
