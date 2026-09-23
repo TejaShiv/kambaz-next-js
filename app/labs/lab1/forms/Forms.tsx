db5a51a6074d572a72a91cd2a0545a83770d3563
Tejas Shivaprasad <tejas.shivaprasad29@gmail.com>
2026-09-23 06:12:24 -0400

    Add Student Profile form covering all Lab 1 field types

--- app/labs/lab1/forms/Forms.tsx
+++ app/labs/lab1/forms/Forms.tsx
@@ -7,6 +7,7 @@ import Checkboxes from "./Checkboxes";
 import Dropdowns from "./Dropdowns";
 import OtherFieldTypes from "./OtherFieldTypes";
 import Buttons from "./Buttons";
+import YourForm from "./YourForm";
 
 export default function Forms() {
   return (
@@ -26,6 +27,7 @@ export default function Forms() {
         <OtherFieldTypes />
         <Buttons />
       </form>
+      <YourForm />
     </div>
   );
 }
