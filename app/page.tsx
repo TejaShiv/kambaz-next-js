b84e362f0fd1728f99289678510071238ead517f
Tejas Shivaprasad <tejas.shivaprasad29@gmail.com>
2026-09-24 02:12:34 -0400

    Add link ids to Labs index and TOC

--- app/page.tsx
+++ app/page.tsx
@@ -1,69 +1,33 @@
-import Image from "next/image";
+import Link from "next/link";
 
-export default function Home() {
+export default function Labs() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert h-5 w-[100px]"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the{" "}
-            <code className="rounded bg-black/[.06] px-1.5 py-0.5 font-mono text-[0.9em] dark:bg-white/[.08]">
-              page.tsx
-            </code>{" "}
-            file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert h-[14px] w-4"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={14}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+    <div id="wd-labs">
+      <h1>Labs</h1>
+      <h2>Tejas Shivaprasad</h2>
+      <ul>
+        <li>
+          <Link href="/labs/lab1" id="wd-lab1-index-link">Lab 1: HTML Examples</Link>
+        </li>
+        <li>
+          <Link href="/labs/lab2" id="wd-lab2-index-link">Lab 2: CSS Basics</Link>
+        </li>
+        <li>
+          <Link href="/labs/lab3" id="wd-lab3-index-link">Lab 3: JavaScript Fundamentals</Link>
+        </li>
+        <li>
+          <Link href="/labs/lab4" id="wd-lab4-link">Lab 4</Link>
+        </li>
+        <li>
+          <Link href="/labs/lab5" id="wd-lab5-link">Lab 5</Link>
+        </li>
+        <li>
+          <Link href="/kambaz" id="wd-kambaz-index-link">Kambaz</Link>
+        </li>
+      </ul>
+      <a href="https://github.com/TejaShiv/kambaz-next-js" id="wd-github">
+        My GitHub repository
+      </a>
     </div>
   );
 }
