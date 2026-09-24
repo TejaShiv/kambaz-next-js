a6fcdf2bd7cb636b83e5b8bfc3a9cf801d595896
Tejas Shivaprasad <tejas.shivaprasad29@gmail.com>
2026-09-24 02:56:53 -0400

    link changes

--- app/labs/TOC.tsx
+++ app/labs/TOC.tsx
@@ -8,7 +8,7 @@ export default function TOC() {
         <li><Link href="/labs/lab1" id="wd-lab1-link">Lab 1</Link></li>
         <li><Link href="/labs/lab2" id="wd-lab2-link">Lab 2</Link></li>
         <li><Link href="/labs/lab3" id="wd-lab3-link">Lab 3</Link></li>
-        <li><Link href="/labs/lab4" id="wd-lab4-toc-link">Lab 4</Link></li>
+        <li><Link href="/labs/lab4" id="wd-lab4-link">Lab 4</Link></li>
         <li><Link href="/labs/lab5" id="wd-lab5-toc-link">Lab 5</Link></li>
         <li><Link href="/kambaz" id="wd-kambaz-link">Kambaz</Link></li>
         <li><Link href="/book/ch1" id="wd-toc-book-link">Chapter 1</Link></li>
